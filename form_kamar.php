cdabba2b6779b43d72f84c9808acb8db42d64db2
mukhlis <mukhlisk93@gmail.com>
2017-01-08 19:29:49 +0700

    My first Commit in git

---
+++ form_kamar.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Form Kamar</title>
+</head>
+<body>
+<h3>Form kamar</h3>
+<form method="GET" action="input_kamar.php">
+	Nomor : <input type="text" name="nomor"><br>
+	ID Hotel : <textarea name="hotel_id"></textarea><br>
+	<input type="submit" value="Simpan">
+</form>
+</body>
+</html>
